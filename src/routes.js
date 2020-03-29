7cde75eb2715c2c2aa9b668303eb4034f70562d3
Caio Favoretto <caio.favoretto@ubus.com.br>
2020-03-29 02:51:43 -0300

    feature: validations added

--- src/routes.js
+++ src/routes.js
@@ -1,20 +1,32 @@
 const express = require('express');
 
 const OngController = require('./controllers/OngController');
+const OngValidator = require('./validators/OngValidator');
+
 const IncidentController = require('./controllers/IncidentController');
+const IncidentValidator = require('./validators/IncidentValidator');
+
 const ProfileController = require('./controllers/ProfileController');
+const ProfileValidator = require('./validators/ProfileValidator');
+
 const SessionController = require('./controllers/SessionController');
+const SessionValidator = require('./validators/SessionValidator');
 
 const routes = express.Router();
 
+routes.post('/sessions', SessionValidator.store, SessionController.store);
+
 routes.get('/ongs', OngController.index);
-routes.post('/ongs', OngController.store);
+routes.post('/ongs', OngValidator.store, OngController.store);
 
-routes.get('/incidents', IncidentController.index);
-routes.post('/incidents', IncidentController.store);
-routes.delete('/incidents/:id', IncidentController.destroy);
+routes.get('/incidents', IncidentValidator.index, IncidentController.index);
+routes.post('/incidents', IncidentValidator.store, IncidentController.store);
+routes.delete(
+  '/incidents/:id',
+  IncidentValidator.destroy,
+  IncidentController.destroy
+);
 
-routes.get('/profile', ProfileController.index);
-routes.post('/sessions', SessionController.store);
+routes.get('/profile', ProfileValidator.index, ProfileController.index);
 
 module.exports = routes;
